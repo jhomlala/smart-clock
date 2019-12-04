005efc28007f149f67b6e7b5032310f5c79e9b6c
Jakub <jhomlala@gmail.com>
2019-12-04 17:22:33 +0100

    Widgets refactor

--- smart_clock/lib/widget/time_text.dart
+++ smart_clock/lib/widget/time_text.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:smart_clock/utils/date_time_utils.dart';
 
+///Widget to display time. It can display time in 24h or 12h format.
 class TimeText extends StatelessWidget {
   static const timeAm = "AM";
   static const timePm = "PM";
 
+  /// 24hour format flag
   final bool is24hourFormat;
+
+  ///DateTime to display
   final DateTime dateTime;
+
+  ///Time font size
   final double timeFontSize;
+
+  ///Padding between time text and "AM/PM"  text
   final double padding;
 
   const TimeText(
-      {Key key, this.is24hourFormat, this.dateTime, this.timeFontSize, this.padding = 0})
-      : super(key: key);
+      {Key key,
+      this.is24hourFormat,
+      this.dateTime,
+      this.timeFontSize,
+      this.padding = 0})
+      : assert(is24hourFormat != null, "Flag can't be null"),
+        assert(dateTime != null, "Date time can't be null"),
+        assert(timeFontSize > 0, " Time font size must be greater than 0"),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +35,13 @@ class TimeText extends StatelessWidget {
     if (!is24hourFormat) {
       hourSuffix = timePm;
     }
-    String time = DateTimeUtils.formatTime(dateTime, is24hourFormat);
+
     List<Widget> widgets = List();
-    widgets.add(Text(time, style: TextStyle(fontSize: timeFontSize)));
-    widgets.add(Padding(padding: EdgeInsets.only(left:padding),));
+    widgets.add(Text(DateTimeUtils.formatTime(dateTime, is24hourFormat),
+        style: TextStyle(fontSize: timeFontSize)));
+    widgets.add(Padding(
+      padding: EdgeInsets.only(left: padding),
+    ));
     if (!is24hourFormat) {
       widgets.add(Text(hourSuffix,
           style: TextStyle(
